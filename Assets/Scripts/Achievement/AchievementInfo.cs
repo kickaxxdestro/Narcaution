ce5fd75d11c6c470f8b6e28284715b0071e35bab
shingliya <damianivan221199@hotmail.com>
2016-08-11 17:06:24 +0800

    stuff

---
+++ Assets/Scripts/Achievement/AchievementInfo.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementInfo {
+
+	public enum Category
+	{
+		C_General,
+		C_Battle,
+		C_Special
+	};
+
+	public string name;
+	public Category category;
+	public string description;
+	public int currentProgress;
+	public int progressNeeded;
+	public bool unlocked;
+
+	public AchievementInfo (string name, Category category, string description, int currentProgress, int progressNeeded, bool unlocked)
+	{
+		this.name = name;
+		this.category = category;
+		this.description = description;
+		this.currentProgress = currentProgress;
+		this.progressNeeded = progressNeeded;
+		this.unlocked = unlocked;
+	}
+}
---
+++ Assets/Scripts/Achievement/AchievementTab.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AchievementTab : MonoBehaviour {
+
+	public float fadeSpeed;
+	public float holdTime;
+
+	enum State
+	{
+		start,
+		hold,
+		end
+	};
+
+	State state;
+
+	// Use this for initialization
+	void Start () 
+	{
+		state = State.start;
+		this.GetComponentInChildren<CanvasGroup> ().alpha = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch (state) 
+		{
+			case State.start:
+				this.GetComponentInChildren<CanvasGroup> ().alpha += Time.unscaledDeltaTime * fadeSpeed;
+				if(this.GetComponentInChildren<CanvasGroup> ().alpha >= 1)
+				{
+					this.GetComponentInChildren<CanvasGroup> ().alpha = 1;
+					state = State.hold;
+				}
+			break;
+
+			case State.hold:
+				holdTime -= Time.unscaledDeltaTime;
+				if (holdTime <= 0)
+					state = State.end;
+			break;
+
+			case State.end:
+				this.GetComponentInChildren<CanvasGroup> ().alpha -= Time.unscaledDeltaTime * fadeSpeed;
+				if(this.GetComponentInChildren<CanvasGroup> ().alpha <= 0)
+				{
+					Destroy (this.gameObject);
+				}
+			break;
+		}
+	}
+
+	public void SetInfo(string name)
+	{
+		gameObject.transform.Find("Name").GetComponent<Text> ().text = name;
+	}
+}
